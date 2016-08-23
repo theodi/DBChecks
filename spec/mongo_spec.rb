a2cd675a526c858b8ccb0d2391390522505b76af
pikesley <github@orgraphone.org>
2016-08-23 15:41:00 +0100

    Test for presence of correct file

--- spec/mongo_spec.rb
+++ spec/mongo_spec.rb
@@ -1,5 +1,6 @@
 describe 'Test Mongo Backups' do
   it 'gets the latest file' do
-    
+    today = (DateTime.now).strftime('%Y-%m-%d')
+    expect(Dir.entries('dumps/').last).to eq "mongo-#{today}.tbz"
   end
 end
