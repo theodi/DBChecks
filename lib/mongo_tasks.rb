abba22bde3d5fa789da4a8cad892d6d31f13f59e
pikesley <github@orgraphone.org>
2016-08-23 16:47:10 +0100

    Does this work in Travis then?

--- lib/mongo_tasks.rb
+++ lib/mongo_tasks.rb
@@ -49,7 +49,7 @@ module DBChecks
 
       print 'Restoring dump... '
 
-      %x[ tar xjf dumps/#{dump} | mongorestore --quiet ]
+      %x[ tar xjf dumps/#{dump} | mongorestore --drop --quiet ]
       FileUtils.rm_r 'dump'
 
       puts 'done'
--- spec/mongo_spec.rb
+++ spec/mongo_spec.rb
@@ -3,4 +3,12 @@ describe 'Test Mongo Backups' do
     today = (DateTime.now).strftime('%Y-%m-%d')
     expect(Dir.entries('dumps/').last).to eq "mongo-#{today}.tbz"
   end
+
+  context 'restores a database' do
+    it 'has collections' do
+      client = Mongo::Client.new(['127.0.0.1:27017'], database: 'govuk_content_publisher')
+      db = client.database
+      expect(db.collections.count).to be >= 10
+    end
+  end
 end
