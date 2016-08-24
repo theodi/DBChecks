22c58172847c7f420a8de9a95746597fe730780e
pikesley <github@orgraphone.org>
2016-08-24 16:55:48 +0100

    Rename

--- spec/quirkafleeg_mongo_spec.rb
+++ spec/quirkafleeg_mongo_spec.rb
@@ -1,4 +1,4 @@
-describe 'Test Mongo Backups' do
+describe 'Test Quirkafleeg Mongo Backups' do
   it 'gets the latest file' do
     today = (DateTime.now).strftime('%Y-%m-%d')
     expect(Dir.entries('dumps/').last).to eq "mongo-#{today}.tbz"
